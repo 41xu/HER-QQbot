e49b34a47cf52fa033e6dd8418047a3e294cf80e
41xu <xu_sy11111@mail.dlut.edu.cn>
2019-09-07 16:28:57 +0800

    更新部分使用说明，规范了代码格式

--- README.md
+++ README.md
@@ -3,4 +3,43 @@ BEJ48-黄恩茹的相关
 
 ## moniter.py
 
-绑定了应援群中的酷Q机器人，通过爬取腾讯小打卡中的集资信息实时监控，进行集资情况的播报。使用cookie模拟登陆
+绑定了BEJ48-黄恩茹应援群中的酷Q机器人，通过爬取腾讯小打卡中的集资信息，进行集资情况的实时播报。使用cookie模拟登陆
+
+### requirement
+
+environment: python3.6 + 酷Q Air/ Pro 
+
+packages:
+
+- requests
+
+- json
+
+- cqhttp
+
+- apscheduler
+
+其中cqhttp的详细介绍参见[项目的官方说明](https://github.com/richardchien/python-cqhttp)
+
+cqhttp只是起到将爬到的消息转发到机器人中的作用，若使用其他机器人消息发送插件则可更换成对应package.
+
+### get start
+
+
+#### Spider Part
+
+爬虫部分使用[Charles](https://www.charlesproxy.com/) 使用方法如下：(macOS+iPhone)
+
+1. 电脑端下载好Charles后，将电脑与手机连接到一个局域网中，在电脑上打开软件，并在手机上点击连接上的Wi-Fi，(iphone用户`点击无线局域网->点击连接上的网络->拉到底点击HTTP代理->配置代理->手动`）HTTP代理中填入电脑IP，端口号填写为8888（默认的）
+
+2. 在Charles的菜单栏中点击`Help -> SSL Proxying -> Install Charles Root Certificate` 在添加证书的弹出窗口中选择添加，此时证书被添加到钥匙串中，但还是不信任状态，因此点进电脑上的钥匙串，搜索到Charles Proxy CA，双击，选择始终信任即可。Mac上的配置完毕。
+
+3. 保证手机和电脑连接到一个网络并且手机上设置好代理之后，在Mac上保持Charles的打开状态，iPhone中浏览器中打开网址`https://chls.pro.ssl.`，网页载入后会弹出添加描述文件的窗口，点击允许开始下载。后在手机`设置->关于本机->证书信任设置->找到Charles CA选择信任`即可。手机配置完毕。
+
+4. 接下来要设置SSL代理。在Charles的菜单栏选择`Proxy -> SSL Proxying Settings -> add `进行要爬取网页的host设置。这里可以顺手把Proxy中的`macOS Proxy`关掉，这个走的是本机的代理，为了抓包的时候便于观察关掉没什么大问题。在添加主机和端口时这里选择add后输入的host为`groupaccount.tenpay.com`端口可以不填默认的是443
+
+**注意⚠️**第四步一定要设置不然Charles看不见https的请求只能看见http请求。
+
+设置好后手机进入微信腾讯小打卡的小程序
+
+tbc
--- moniter.py
+++ moniter.py
@@ -21,90 +21,92 @@ cookies = {
     'grp_qluin': '05b8205814986aae4782657e03cc071b',
     'grp_qlskey': 'v0ae789cc105d720d75fd79d4f640594',
 }
-data={
-    'guid':'kqptEkGo0100001440036668',
-    'offset':'0',
-    'limit':'10',
-    'flag':'0',
+# 使用cookies登陆，不登陆无法查看粉丝圈（所以还得先用这个账号手动加入粉丝圈）咱也不知道这个cookies的过期时间是多少...
+data = {
+    'guid': 'kqptEkGo0100001440036668',  # 指向这个集资项目的集资情况页面
+    'offset': '0',
+    'limit': '10',  # response 的集资金额条数，limit可调，默认10条
+    'flag': '0',
 }
-u="https://groupaccount.tenpay.com/fcgi-bin/grp_project_qry_info.fcgi"
-datatotal={
-    "project_id":"7TAnLHtb0101440037495668",
+u = "https://groupaccount.tenpay.com/fcgi-bin/grp_project_qry_info.fcgi"
+datatotal = {
+    "project_id": "7TAnLHtb0101440037495668",
 }
+
 
 def timechange(t):
-    stamp=time.mktime(time.strptime(t,'%Y-%m-%d %H:%M:%S'))
+    stamp = time.mktime(time.strptime(t, '%Y-%m-%d %H:%M:%S'))
     return int(stamp)
 
-def curent(u,headers,cookies,datatotal):
-    response=requests.post(url=u,headers=headers,cookies=cookies,data=datatotal,verify=False)
-    content=response.content
-    result=json.loads(content)
-   # print(result)
-    cur={
-        "total_amount":result['total_amount'][:-2]+"."+result['total_amount'][-2:],
-        "percent":result['percent'],
-        "target_amount":result['target_amount'][:-2]+"."+result['target_amount'][-2:],
+
+def curent(u, headers, cookies, datatotal):
+    response = requests.post(url=u, headers=headers, cookies=cookies, data=datatotal, verify=False)
+    content = response.content
+    result = json.loads(content)
+    # print(result)
+    cur = {
+        "total_amount": result['total_amount'][:-2] + "." + result['total_amount'][-2:],
+        "percent": result['percent'],
+        "target_amount": result['target_amount'][:-2] + "." + result['target_amount'][-2:],
     }
     return cur
 
 
-def moniter(url,headers,cookies,data,lasttime):
-
-    response = requests.post(url=url, headers=headers,cookies=cookies, data=data,verify=False)
+def moniter(url, headers, cookies, data, lasttime):
+    response = requests.post(url=url, headers=headers, cookies=cookies, data=data, verify=False)
     try:
         content = response.content
-        queue=[]
+        queue = []
 
         results = json.loads(content)
 
-        if timechange(lasttime)<timechange(results['list_json'][0]['create_time']):
-        # 有人集资啦！更新上次集资时间再更新一下要播报出去的集资情况！
+        if timechange(lasttime) < timechange(results['list_json'][0]['create_time']):
+            # 有人集资啦！更新上次集资时间再更新一下要播报出去的集资情况！
             for x in results['list_json']:
-                if timechange(x['create_time'])>timechange(lasttime):
+                if timechange(x['create_time']) > timechange(lasttime):
                     queue.append({
-                        'nickname':x['nickname'],
-                        'creagte_time':x['create_time'],
-                        'msg_content':x['msg_content'],
+                        'nickname': x['nickname'],
+                        'creagte_time': x['create_time'],
+                        'msg_content': x['msg_content'],
                     })
 
-            lasttime=results['list_json'][0]['create_time']
+            lasttime = results['list_json'][0]['create_time']
         return queue, lasttime
     except:
-        return [],lasttime
+        return [], lasttime
 
 
-def broadcast(queue,cur):
-    res=""
-    if queue==[]:
+def broadcast(queue, cur):
+    res = ""
+    if queue == []:
         return res
     for x in queue:
-        res=res+"感谢: "+x['nickname']+" "+x['msg_content']+"\n"
-    res+="当前金额： "+cur['total_amount']+" 目前的集资进度已达： "+cur['percent']+"%\n"
-    res+="目标金额： "+cur['target_amount']+" 大家请多多支持茹茹的B50吧！"
+        res = res + "感谢: " + x['nickname'] + " " + x['msg_content'] + "\n"
+    res += "当前金额： " + cur['total_amount'] + " 目前的集资进度已达： " + cur['percent'] + "%\n"
+    res += "目标金额： " + cur['target_amount'] + " 大家请多多支持茹茹的B50吧！"
 
     return res
 
+
 def block():
     global lasttime
-    queue,lasttime=moniter(url,headers,cookies,data,lasttime)
-    cur=curent(u,headers,cookies,datatotal)
-    message=broadcast(queue,cur)
-    if queue!=[]:
-        bot.send_group_msg(group_id=477218146,message=message)
+    queue, lasttime = moniter(url, headers, cookies, data, lasttime)
+    cur = curent(u, headers, cookies, datatotal)
+    message = broadcast(queue, cur)
+    if queue != []:
+        bot.send_group_msg(group_id=477218146, message=message)
 
-if __name__=='__main__':
-    lasttime = '2019-09-06 12:30:00' # initial lasttime
-    bot = CQHttp(api_root='http://127.0.0.1:5700/',access_token='lovely|teemo',secret='lovely|teemo')
-    second=60 # 60s扫一次集资情况
+
+if __name__ == '__main__':
+    lasttime = '2019-09-06 12:30:00'  # initial lasttime
+    bot = CQHttp(api_root='http://127.0.0.1:5700/', access_token='lovely|teemo', secret='lovely|teemo')
+    second = 60  # 60s扫一次集资情况
     # second=3600*2 # 2h扫一次
-    schedule=BlockingScheduler()
-    schedule.add_job(block,'interval',seconds=second)
+    schedule = BlockingScheduler()
+    schedule.add_job(block, 'interval', seconds=second)
     schedule.start()
 
 #    queue,lasttime=moniter(url,headers,cookies,data,lasttime) # initial queue,lasttime
 #    cur=curent(u,headers,cookies,datatotal)
 #    message=broadcast(queue,cur)
 #    print(message)
-
-
