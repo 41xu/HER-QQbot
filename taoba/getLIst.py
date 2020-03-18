88b3b37206026b6d41369254857203dbeaaf4fdf
41xu <xu_sy11111@mail.dlut.edu.cn>
2020-03-18 14:27:34 +0800

    update getList func

--- taoba/getLIst.py
+++ taoba/getLIst.py
@@ -8,7 +8,13 @@ import time
 post出错时的response
 {'code': 'id', 'msg': '参数id应该大于1.'}
 """
-fault = "62$XZyIVn/Oe0lxsgzKaVH0UZktCgd0YxRNzJOnYn/Nfs0uMhOAi1Ruk7tSvqRgUwTbjMgZpvIkhiKRUAmpSAAG7ziB"
+
+"""
+config:{'requestTime': 1584453397313, 'pf': 'h5'}, url: https://www.tao-ba.club/config -> response null
+detail:{'id': '1322', 'requestTime': 1584453397381, 'pf': 'h5'}, url: https://www.tao-ba.club/idols/detail
+buystates:{'yyaid': '1322', 'requestTime': 1584453397489, 'pf': 'h5'}, url: https://www.tao-ba.club/idols/join/buystats -> resoponse null
+join:{'ismore': False, 'limit': 15, 'id': '1322', 'offset': 0, 'requestTime': 1584457188679, 'pf': 'h5'}, url: https://www.tao-ba.club/idols/join
+"""
 
 
 def addSalt(ori: bytearray):
@@ -51,25 +57,19 @@ def decodeData(ori: str):
     return json.loads(Result)
 
 
-"""
-config:{'requestTime': 1584453397313, 'pf': 'h5'}, url: https://www.tao-ba.club/config -> response null
-detail:{'id': '1322', 'requestTime': 1584453397381, 'pf': 'h5'}, url: https://www.tao-ba.club/idols/detail
-buystates:{'yyaid': '1322', 'requestTime': 1584453397489, 'pf': 'h5'}, url: https://www.tao-ba.club/idols/join/buystats -> resoponse null
-join:{'ismore': False, 'limit': 15, 'id': '1322', 'offset': 0, 'requestTime': 1584457188679, 'pf': 'h5'}, url: https://www.tao-ba.club/idols/join
-"""
-
 header = {
     'Content-Type': 'application/json',
     'Origin': 'https://www.tao-ba.club',
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Host': 'www.tao-ba.club',
+    'Cookie': 'l10n=zh-cn',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15',
     'Referer': 'https://www.tao-ba.club/',
     'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive',
 }
-cookie={
-    'l10n':'zh-cn',
+cookie = {
+    'l10n': 'zh-cn',
 }
 
 """
@@ -81,16 +81,66 @@ response.content -> byte type
 
 def getInfo(id):
     url = "https://www.tao-ba.club/idols/detail"
-    # url="https://www.tao-ba.club/#/pages/idols/detail?id={}".format(id)
-    data = "{{'id':'{}','requestTime':{},'pf':'h5'}}".format(id, int(time.time() * 1000))
+    data = '{{"id": "{}", "requestTime": {}, "pf": "h5"}}'.format(id, int(time.time() * 1000))
     data = encodeData(data)
-    response = requests.post(url=url,headers=header, data=data,cookies=cookie)
-    response = response.text
-    print(response)
-    print(decodeData(response))
-    return decodeData(response)
+    response = requests.post(url=url, headers=header, data=data, cookies=cookie)
+    response = decodeData(response.text)
+    # res+=response['datas']['title']+str(response['datas']['sellstats'])+str(response['datas']['donation'])
+    res = response['datas']['title'] + " 当前集资金额：¥" + str(response['datas']['donation']) + "\n"
+    return res
+
+
+"""
+join:{'ismore': False, 'limit': 15, 'id': '1322', 'offset': 0, 'requestTime': 1584457188679, 'pf': 'h5'}
+url: https://www.tao-ba.club/idols/join
+page: limit num in per page
+"""
+page = 20
+total = {}  # user id->money
+dic = {}  # user info
+
+def getList(id, page):
+    url = "https://www.tao-ba.club/idols/join"
+    base = '{{"ismore":{},"limit":{},"id":"{}","offset":{},"requestTime":{},"pf":"h5"}}'
+    flag = False
+    offset = 0
+    while True:
+        if flag:
+            f = "true"
+        else:
+            f = "false"
+        data = encodeData(base.format(f, page, id, offset, int(time.time() * 1000)))
+        response = decodeData(requests.post(url=url, headers=header, data=data).text)["list"]
+        for x in response:
+            dic[x['userid']] = [x['id'], x['money'], x['nick'], x['stime']]
+        flag = len(response) == 15
+        offset += page
+        if flag == False:
+            break
+    return dic
+
+
+def process(dic,last):
+    print(dic)
+    print(last)
+    text=""
+    for x in dic.keys():
+        if x not in last.keys():
+            text+="感谢"+dic[x][2]+"支持¥"+str(dic[x][1])+"元！\n"
+        elif dic[x][1]>last[x][1]:
+            text+="感谢"+dic[x][2]+"支持¥"+str(dic[x][1]-last[x][1])+"元！\n"
+    print(text)
+    return text
 
 
 if __name__ == '__main__':
-    getInfo(1322)
-    print(decodeData("51$XZyIVv9MZbJ2Mmo2F1L0UXlKaSwLLXAJiMxnVYcyBrUTMXG1B7YVNBIx5lEeSCeqlTBwqiMAPyFJoA=="))
+    res = getInfo(1322)
+    print(res)
+    last={}
+    while 1:
+        dic=getList(1322,15)
+        process(dic,last)
+        last=dic
+        time.sleep(60)
+
+
